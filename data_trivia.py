0885d7649f2e36533c93aa6836468e1717a3bdc8
SHofRiviaa <salmanhaidar9576@gmail.com>
2024-03-08 17:50:40 -0500

    added data trivia logic

---
+++ data_trivia.py
@@ -0,0 +1,34 @@
+import requests
+
+TRIVIA_API_URL = "https://opentdb.com/api.php"
+
+class Data_Trivia_Builder:
+
+    def __init__(self, num_qs, category, difficulty, type):
+        
+        self.setup_parameter(num_qs, category, difficulty, type)
+
+    def setup_parameter(self, num_qs, category, difficulty, type):
+
+        self.num_qs = num_qs
+        self.category = category
+        self.difficulty = difficulty
+        self.type = type = type
+
+        self.parameters = {
+            "amount": self.num_qs,
+            "category": self.category,
+            "difficulty": self.difficulty,
+            "type": self.type
+        }
+
+    def get_trivia_questions(self):
+
+        response = requests.get(TRIVIA_API_URL, params = self.parameters)
+        response.raise_for_status()
+        data = response.json()
+        trivia_data = data['results']
+
+        return trivia_data
+
+
