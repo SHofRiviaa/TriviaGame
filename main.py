7d79aa7b8263672e4c3baed5d832b8761ec1b2ba
SHofRiviaa <salmanhaidar9576@gmail.com>
2024-03-09 16:07:59 -0500

    added get user options logic, need to optimize later

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from game_gui import Trivia_GUI
+from user_options import UserOptions
 
 
 def main():
+
+    user_options = UserOptions()
+    user_options.get_user_options_values()
+
     game = Trivia_GUI()
     
 
--- user_options.py
+++ user_options.py
@@ -4,10 +4,14 @@ class UserOptions:
 
         self.user_options = {}
 
-    def get_user_options(self):
-        pass
+    def get_user_options_values(self):
+        print("Welcome to the Trivia Game!")
+
+        self.get_number_of_questions()
+        self.get_category()
+        self.get_difficulty()
+        self.get_trivia_type()
         
-    
     def get_number_of_questions(self):
         while True:
             try:
@@ -30,13 +34,34 @@ class UserOptions:
                                      "14 = Television, 15 = Video Games, 16 = Board Games, " +
                                      "17 = Science & Nature, 18 = Computers, 19 = Mathematics"))
                 
-                if category > 9 and category <= 50:
+                if category >= 9 and category <= 19:
                     self.user_options['category'] = category
                     break
                 else:
                     print("Please enter a number greater than 9 and less than 50")
             except ValueError:
                 print("Please enter a number")
+
+    def get_difficulty(self):
+        while True:
+            difficulty = input("Select a difficulty: [easy, medium, hard] ")
+            if difficulty in ['easy', 'medium', 'hard']:
+                self.user_options['difficulty'] = difficulty
+                break
+            else:
+                print("Please enter easy, medium or hard")
+    
+    def get_trivia_type(self):
+        while True:
+            type = input("Select a type: [multiple, boolean] ")
+            if type in ['multiple', 'boolean']:
+                self.user_options['type'] = type
+                break
+            else:
+                print("Please enter multiple or boolean")
+
+    def get_user_options(self):
+        return self.user_options
 
     
 
